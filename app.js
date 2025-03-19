05611fee47a4ae4e3cb11bf5be2f8dbc2dd5e603
dipu kumar <dipukumardevcod@email.com>
2025-03-19 11:46:17 +0530

    update the footer

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
-// Redirect function when clicking anywhere on the page
-function redirectToTelegram() {
-    window.location.href = "https://t.me/+TrCzDy-NPME5YTU1";
-}
+// // Redirect function when clicking anywhere on the page
+// function redirectToTelegram() {
+//     window.location.href = "https://t.me/+TrCzDy-NPME5YTU1";
+// }
 
-// Auto redirect after 3 seconds
-// setTimeout(redirectToTelegram, 3000);
+// // Auto redirect after 3 seconds
+// // setTimeout(redirectToTelegram, 3000);
 
-setTimeout(function() {
-    window.location.href = "https://t.me/+TrCzDy-NPME5YTU1";
-}, 3000);
+// setTimeout(function() {
+//     window.location.href = "https://t.me/+TrCzDy-NPME5YTU1";
+// }, 3000);
+
