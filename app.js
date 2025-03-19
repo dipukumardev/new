521f2418878f104eae260782710a86d63396a9ee
dipu kumar <dipukumardevcod@email.com>
2025-03-19 11:25:33 +0530

    updat the code

--- app.js
+++ app.js
@@ -1,4 +1,11 @@
-// Auto redirect to Telegram after 3 seconds
+// Redirect function when clicking anywhere on the page
+function redirectToTelegram() {
+    window.location.href = "https://t.me/+TrCzDy-NPME5YTU1";
+}
+
+// Auto redirect after 3 seconds
+// setTimeout(redirectToTelegram, 3000);
+
 setTimeout(function() {
     window.location.href = "https://t.me/+TrCzDy-NPME5YTU1";
 }, 3000);
